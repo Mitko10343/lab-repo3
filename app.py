21c6005a0cea7c4024d60408cfb8103eb94bd4af
Dimiter <c15334276@mydit.ie>
2017-10-04 15:45:37 +0000

    commit2

--- app.py
+++ app.py
@@ -16,9 +16,15 @@ mysql.init_app(app)
 @app.route("/")
 def hello(): # Name of the method
     cur = mysql.connection.cursor() #create a connection to the SQL instance
-    cur.execute('''SELECT * FROM students''') # execute an SQL statment
+    cur.execute('''SELECT * FROM students''')
+    cur.execute('''INSERT INTO students (studentName,email) values ("Student1","Student1@mydit.ie")''')#add a student record
+    cur.execute('''update student set studentName = 'Updated Name' WHERE studentName="Student1" ''')# update student record
+    cur.execute('''delete from student where studentName= 'Updated Name''') #delete from table
     rv = cur.fetchall() #Retreive all rows returend by the SQL statment
+    return app.send_static_file('static.html')#return a static html file
     return str(rv)      #Return the data in a string format
 if __name__ == "__main__":
-        app.run(host='0.0.0.0', port='5000') #Run the flask app at port 5000
+        app.run(host='0.0.0.0', port='5000'); #Run the flask app at port 50
+      
+        
 
